6c91bfbec39bf4e450c0392d70493b45d71ce2fb
abhishek peiris <abhishekdananjaya1@yahoo.com>
2023-04-27 23:23:16 +0530

    add model to the project

--- app/consumers/data_consumer.py
+++ app/consumers/data_consumer.py
@@ -1,11 +1,14 @@
 import pika
 from pika.exchange_type import ExchangeType
+import time
 
 exchangeName = "pyropro"
 firePredictionRoutingKey = "PyroproFirePredictionQueue"
 
 def on_message_received(ch, method, properties, body):
     print(f'Payments - received new message: {body}')
+    time.sleep(5)
+    print("Wait is over.")
 
 connection_parameters = pika.ConnectionParameters('localhost')
 
@@ -14,11 +17,11 @@ connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
 channel.exchange_declare(exchange=exchangeName, exchange_type=ExchangeType.direct, durable=True)
-#
+
 queue = channel.queue_declare(queue=firePredictionRoutingKey, durable=True)
 
 channel.queue_bind(exchange=exchangeName, queue=queue.method.queue, routing_key=firePredictionRoutingKey)
-# channel.queue_bind(exchange='pyropro', queue=queue.method.queue, routing_key='both')
+
 channel.basic_consume(queue=firePredictionRoutingKey, auto_ack=True,
     on_message_callback=on_message_received)
 
